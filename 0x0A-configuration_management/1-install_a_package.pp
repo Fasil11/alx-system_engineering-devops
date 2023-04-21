82b41f3e078036c6bdfba82698670ca16d883a6c
Fasil11 <seifufasil@gmail.com>
2023-04-21 13:50:41 -0700

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint -v 2.5.0
+#Using Puppet, install flask from pip
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
-}
+ exec { 'install-puppet-lint':
+ command => '/usr/bin/apt-get -y install puppet-lint',
+ require => [Package['puppet-lint'], Exec['apt-update']],
+ }
