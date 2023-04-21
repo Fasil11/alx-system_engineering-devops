f565bb16b4d5ad255f0ba12d57002b9f994983f9
Fasil11 <seifufasil@gmail.com>
2023-04-21 13:03:04 -0700

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
 # install puppet-lint -v 2.5.0
 
-package { 'puppet-lint': ensure => '2.5.0', }
- exec { 'apt-update': command => '/usr/bin/apt-get update',
- refreshonly => true, }
- exec { 'install-puppet-lint': 
- command => '/usr/bin/apt-get -y install puppet-lint',
- require => [Package['puppet-lint'],
- Exec['apt-update']], }
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+}
