3a14e1e6e109964c4f77686eaf85a445fdccf28c
Fasil11 <seifufasil@gmail.com>
2023-07-14 10:01:12 -0700

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,5 @@
-# Increse the request's limit
-
-# Increse ulimit value
-exec { 'fix-config-nginx':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-}
-
-# Restart nginex service
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+# Change nginx file limit
+exec { 'Change nginx limit':
+  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+  provider => shell,
 }
